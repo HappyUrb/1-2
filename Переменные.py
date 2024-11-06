08982563e87e1578163ee627ffd5706487005058
HappyUrb <nik.skutin205@gmail.com>
2024-11-06 17:55:12 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+hw_count =12
+time_count =1.5
+course_name ='Python,'
+h_name = time_count / hw_count
+print ('Курс:',course_name, 'всего задач:',hw_count,',затрачено часов:',time_count,',среднее время выполнения:',h_name)
