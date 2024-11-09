d90f5ea8249ba4e0c1e90a86d1f7a3bc097ba611
HappyUrb <nik.skutin205@gmail.com>
2024-11-09 18:31:16 +0300

    Add files via upload

---
+++ Переменные.py
@@ -0,0 +1,5 @@
+homework = 12
+time_count = 1.5
+course_name = 'Python'
+h_name = time_count / homework
+print ('Курс:' + str(course_name) + ', всего задач:' + str(homework) + ', затрачено часов: ' + str(time_count) + ', среднее время выполнения:', h_name, 'часа.')
